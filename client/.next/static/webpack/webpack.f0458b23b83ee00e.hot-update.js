9ed203097e088f33045254abe3862b2a2c0c29e2
sparshshrma <sparsh0508s@gmail.com>
2023-06-20 15:24:47 -0600

    made few changes

--- client/.next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ client/.next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3838ce8d73294853"; }
+/******/ 	__webpack_require__.h = function() { return "7d082160e6928659"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
