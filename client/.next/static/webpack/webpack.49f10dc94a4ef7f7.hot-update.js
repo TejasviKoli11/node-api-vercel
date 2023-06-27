754c4c1605aefebb4055434457f18a0da89f013c
sparshshrma <sparsh0508s@gmail.com>
2023-06-27 00:34:44 -0600

    Uservents Page created

--- client/.next/static/webpack/webpack.49f10dc94a4ef7f7.hot-update.js
+++ client/.next/static/webpack/webpack.49f10dc94a4ef7f7.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "050bf42111d3e247"; }
+/******/ 	__webpack_require__.h = function() { return "eaa869fb04cc4214"; }
 /******/ }();
 /******/ 
 /******/ }
--- client/pages/uservents.js
+++ client/pages/uservents.js
@@ -1,26 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 import axios from 'axios';
 
-const UsersPage = ({ users }) => {
+const HomePage = () => {
+  const [roomData, setRoomData] = useState([]);
+  const [buildingData, setBuildingData] = useState([]);
+
+  useEffect(() => {
+    const fetchRoomData = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/rooms');
+        const data = response.data;
+        setRoomData(data);
+      } catch (error) {
+        console.error('Error fetching room data:', error);
+      }
+    };
+
+    fetchRoomData();
+  }, []);
+
+  useEffect(() => {
+    const fetchBuildingData = async () => {
+      try {
+        const response = await axios.get('http://localhost:4000/buildings');
+        const data = response.data;
+        setBuildingData(data);
+      } catch (error) {
+        console.error('Error fetching building data:', error);
+      }
+    };
+
+    fetchBuildingData();
+  }, []);
+
   return (
     <div>
-      <h1>User List:</h1>
+      <h1>Room Data</h1>
       <ul>
-        {users.map((user) => (
-          <li key={user._id}>{user.username}</li>
+        {roomData.map((room) => (
+          <li key={room._id}>
+            <p>Name: {room.name}</p>
+            <p>Building: {room.building}</p>
+            <p>Floor: {room.floor}</p>
+          </li>
         ))}
       </ul>
+
+      <h1>Building Data</h1>
+      <ul>
+        {buildingData.map((building) => (
+          <li key={building._id}>
+            <p>Name: {building.name}</p>
+            <p>Floors: {building.floors}</p>
+          </li>
+        ))}
+      </ul>
+      <Link href="/login">
+            <p> Login </p>
+                </Link>
+      
     </div>
   );
 };
 
-export async function getServerSideProps() {
-  const response = await axios.get('http://localhost:3000/api/user');
-  const users = response.data;
 
-  return {
-    props: {
-      users,
-    },
-  };
-}
+export default HomePage;
