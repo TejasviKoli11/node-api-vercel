396cba6c25b6ffaf35065ca05cc03da3709ee97b
TejasviKoli11 <tejasvipraveen.koli@edu.sait.ca>
2023-07-04 09:42:18 -0600

    Login, signup, technician

--- client/pages/login.js
+++ client/pages/login.js
@@ -3,10 +3,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import Link from "next/link";
 import styles from "./login.module.css";
-<<<<<<< HEAD
 import { FaHome, FaPhone, FaEnvelope, FaFax } from 'react-icons/fa';
-=======
->>>>>>> 71cdc380edf5b9a22f6c121c58ce6ade41b30a92
 
 export default function LoginPage() {
   const [errorMessage, setErrorMessage] = useState({});
