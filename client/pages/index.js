3321db688cf6e89c82acf9dc1e8b7aa4d1373b01
sparshshrma <sparsh0508s@gmail.com>
2023-07-03 20:21:19 -0600

    ..

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,19 +2,19 @@ import Link from 'next/link';
 import styles from './index.module.css';
 import { FaHome, FaPhone, FaEnvelope, FaFax } from 'react-icons/fa';
 
-
 function IndexPage() {
-    return (
-        <div>
-          <nav className={styles.siteNavigation}>
-            <div className={styles.logo}>
-              <Link href="/">
-                  <button className={styles.logoButton}>Smart Vents</button>
-              </Link>
-            </div>
-            <div className={styles.menu}>
-            <Link href="/login">
+  return (
+    <div>
+      <nav className={styles.siteNavigation}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <button className={styles.logoButton}>Smart Vents</button>
+          </Link>
+        </div>
+        <div className={styles.menu}>
+          <Link href="/login">
             <p className={styles.button}> Login </p>
+<<<<<<< HEAD
                 </Link>
                 <Link href="/signup">
                 <p className={styles.button}> Sign Up </p>
@@ -47,10 +47,22 @@ function IndexPage() {
         <h4>Vent Status</h4>
         <div className="vent-icon">
           <img src="vent_icon.jpg" alt="Vent" />
+=======
+          </Link>
+          <Link href="/signup">
+            <p className={styles.button}> Sign Up </p>
+          </Link>
+          <Link href="/contact">
+            <p className={styles.button}> Contact Us </p>
+          </Link>
+          <Link href="/about">
+            <p className={styles.button}> About Us </p>
+          </Link>
+>>>>>>> dbf6a679163a43a0fd562c7e6ef6c04aeb3bf7fc
         </div>
-      </div>
-    </div>
+      </nav>
 
+<<<<<<< HEAD
 
           <div>
                 <footer className={styles.footer}>
@@ -110,15 +122,122 @@ function IndexPage() {
               </ul>
             </div>
           </footer>
-          </div>
+=======
+      <div className="container">
+        <img className={styles.photo} src='./familyPhoto.png' alt='family photo' style={{ width: "100%" }} />
+        <div className="text">Make your life easy, with our Smart vent application</div>
+      </div>
 
-
+      <div className="dashboard">
+        <div className="building">
+          <h2>Building Name</h2>
+          <img src="building_image.jpg" alt="Building" />
         </div>
+        <div className="room">
+          <h3>Room Name</h3>
+          <img src="room_image.jpg" alt="Room" />
+        </div>
+        <div className="vent">
+          <h4>Vent Status</h4>
+          <div className="vent-icon">
+            <img src="vent_icon.jpg" alt="Vent" />
+>>>>>>> dbf6a679163a43a0fd562c7e6ef6c04aeb3bf7fc
+          </div>
+        </div>
+      </div>
 
+      <div className={styles.menu}>
+        <Link href="./server/routes/api/BuildingRouting.js">
+          <p className={styles.button}> Building Routing </p>
+        </Link>
+        <Link href="/RoomRoserver/routes/api/RoomRouting.js">
+          <p className={styles.button}> Room Routing </p>
+        </Link>
+        <Link href="/server/routes/api/RoomRouting.js">
+          <p className={styles.button}> Vent Data </p>
+        </Link>
+      </div>
 
+      <div className="container">
+        <img className={styles.photo} src='./familyPhoto.png' alt='family photo' style={{ width: "100%" }} />
+        <div className="text">Make your life easy, with our Smart vent application</div>
+      </div>
 
+      <div className="dashboard">
+        <div className="building">
+          <h2>Building Name</h2>
+          <img src="building_image.jpg" alt="Building" />
+        </div>
+        <div className="room">
+          <h3>Room Name</h3>
+          <img src="room_image.jpg" alt="Room" />
+        </div>
+        <div className="vent">
+          <h4>Vent Status</h4>
+          <div className="vent-icon">
+            <img src="vent_icon.jpg" alt="Vent" />
+          </div>
+        </div>
+      </div>
 
-      );
-    }
-  
+      <div>
+        <footer className={styles.footer}>
+          <div className={styles.column}>
+            <h3 id={styles.h3f}>About Us</h3>
+            <p id={styles.pf}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consequat tellus eget sapien mollis, eu efficitur orci aliquet.
+            </p>
+          </div>
+          <div className={styles.column}>
+            <h3 id={styles.h3f}>Support</h3>
+            <ul id={styles.ulf}>
+              <li id={styles.lif}>How to Install</li>
+              <li id={styles.lif}>FAQ</li>
+              <li id={styles.lif}>Published Research</li>
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h3 id={styles.h3f}>Company</h3>
+            <ul id={styles.ulf}>
+              <li id={styles.lif}>
+                <a href="/" id={styles.af}>Home</a>
+              </li>
+              <li id={styles.lif}>
+                <a href="/about" id={styles.af}>About Us</a>
+              </li>
+              <li id={styles.lif}>
+                <a href="/contact" id={styles.af}>Contact Us</a>
+              </li>
+              <li id={styles.lif}>
+                <a href="/services" id={styles.af}>Services</a>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.column}>
+            <h3 id={styles.h3f}>Contact Us</h3>
+            <ul id={styles.ulf}>
+              <li id={styles.lif}>
+                <FaHome className={styles.icon} />
+                123 Main Street, City, Country
+              </li>
+              <li id={styles.lif}>
+                <FaPhone className={styles.icon} />
+                +123 456 7890
+              </li>
+              <li id={styles.lif}>
+                <FaEnvelope className={styles.icon} />
+                info@example.com
+              </li>
+              <li id={styles.lif}>
+                <FaFax className={styles.icon} />
+                +123 456 7891
+              </li>
+            </ul>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+}
+
 export default IndexPage;
--- client/pages/login.js
+++ client/pages/login.js
@@ -1,8 +1,7 @@
-import Link from 'next/link';
-import  styles from './login.module.css';
-import { FaHome, FaPhone, FaEnvelope, FaFax } from 'react-icons/fa';
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 import axios from "axios";
+<<<<<<< HEAD
 
 //import React from 'react'
 
@@ -31,93 +30,49 @@ import React, {useState} from "react";
       }
   }
 
+=======
+import Link from "next/link";
+import styles from "./login.module.css";
+>>>>>>> dbf6a679163a43a0fd562c7e6ef6c04aeb3bf7fc
 
 export default function LoginPage() {
-  const Login = ({setLoginUser})=> {
-    const Login = ({setLoginUser}) => {
-      const history = useHistory()
-        const [user,setUser] = useState({
-          name:"",
-          password:""
-        })
-        const handleChange = e => {
-          const {name,value} = e.target
-          setUser({
-            ...user,
-            [name]:value
-          })
-        }
-  
-        const login = () =>{
-          axios.post("http://localhost:4000/login",user)
-          .then(res=>setLoginUser(res.data.user))
-        }
-    }
-  }
-  
+  const [errorMessage, setErrorMessage] = useState({});
+  const [isSubmitted, setSubmitted] = useState(false);
+  const router = useRouter();
 
-    //react states for checking
-    const[errorMessage, setErrorMessage] = useState({});
-    const [isSubmitted, setSubmitted] = useState(false);
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-    //User login, providing database for checking purpose
+    const { uname, pass } = event.target.elements;
 
     const database = [
-        {
-            username : "Tejasvi",
-            password: "password"
-        },
-        {
-            username : "Aman",
-            password: "password"
-        },
-        {
-            username : "Sparsh",
-            password: "password"
-        },
+      {
+        username: "Tejasvi",
+        password: "password",
+      },
+      {
+        username: "Aman",
+        password: "password",
+      },
+      {
+        username: "Sparsh",
+        password: "password",
+      },
     ];
 
-    //for error - if wrong password/username
-    const errors = {
-        uname: "Invalid username",
-        pass: "Invalid password"
-      };
-      //----------------------------------
-      const router = useRouter();
-    //----------------------------------
+    const userData = database.find((user) => user.username === uname.value);
 
-      const handleSubmit = (event) => {
-
-        
-        //Prevent page reload
-        event.preventDefault();
-    
-        var { uname, pass } = document.forms[0];
-    
-        // Find user login info
-        const userData = database.find((user) => user.username === uname.value);
-    
-        // Compare user info
-        if (userData) {
-          if (userData.password !== pass.value) {
-            // Invalid password
-            setErrorMessage({ name: "pass", message: errors.pass });
-          } else {
-            setSubmitted(true);
-            //console.log("Hello ", uname.value);
-
-            //-----------------------------------
-            if (userData.username === "Aman"){
-              router.push('/uservents');
-            } else {
-              router.push('/technicianVentInfo');
-            }
-            //-------------------------------------
-          }
+    if (userData) {
+      if (userData.password !== pass.value) {
+        setErrorMessage({ name: "pass", message: "Invalid password" });
+      } else {
+        setSubmitted(true);
+        if (userData.username === "Aman") {
+          router.push("/uservents");
         } else {
-          // Username not found
-          setErrorMessage({ name: "uname", message: errors.uname });
+          router.push("/technicianVentInfo");
         }
+<<<<<<< HEAD
       };
     
       //  code for error message
@@ -186,5 +141,87 @@ export default function LoginPage() {
           </div>
         </div>
     </div>
+=======
+      }
+    } else {
+      setErrorMessage({ name: "uname", message: "Invalid username" });
+    }
+  };
+
+  const renderErrorMessage = (name) =>
+    name === errorMessage.name && (
+      <div className={styles.error}>{errorMessage.message}</div>
+>>>>>>> dbf6a679163a43a0fd562c7e6ef6c04aeb3bf7fc
     );
+
+  const renderForm = (
+    <div>
+      <div className={styles.form}>
+        <form onSubmit={handleSubmit}>
+          <div className={styles.inputcontainer}>
+            <label>Username</label>
+            <input
+              type="text"
+              name="uname"
+              required
+              className={styles.label}
+            />
+            {renderErrorMessage("uname")}
+          </div>
+          <div className={styles.inputcontainer}>
+            <label>Password</label>
+            <input
+              type="password"
+              name="pass"
+              required
+              className={styles.label}
+            />
+            {renderErrorMessage("pass")}
+          </div>
+          <div className={styles.buttoncontainer}>
+            <input type="submit" value="Login" />
+          </div>
+        </form>
+        <Link href="/forgotpass" className={styles.fgtpass}>
+          Forgot Password?
+        </Link>
+      </div>
+    </div>
+  );
+
+  return (
+    <div>
+      <nav className={styles.siteNavigation}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <button className={styles.logoButton}>Smart Vents</button>
+          </Link>
+        </div>
+        <div className={styles.menu}>
+          <Link href="/">
+            <p className={styles.button}> Home </p>
+          </Link>
+          <Link href="/signup">
+            <p className={styles.button}> Sign Up </p>
+          </Link>
+          <Link href="/contact">
+            <p className={styles.button}> Contact Us </p>
+          </Link>
+          <Link href="/about">
+            <p className={styles.button}> About Us </p>
+          </Link>
+        </div>
+      </nav>
+      <div className={styles.app}>
+        <div className={styles.loginform}>
+          <div className={styles.title}>Login</div>
+          {isSubmitted ? (
+            <div>User is successfully logged in</div>
+          ) : (
+            renderForm
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
