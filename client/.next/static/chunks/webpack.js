87e3d8462c6fce9a0e4ea2cae1578e8cf285cb27
Aman0373 <amandeepkaur0373@gmail.com>
2023-07-05 00:26:19 -0600

    Dashboard Done

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0330eaede69bbe90"; }
+/******/ 		__webpack_require__.h = function() { return "613b01beda714b73"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/dashboard.js
+++ client/pages/dashboard.js
@@ -4,51 +4,39 @@ import React from 'react';
 
 import styles from './dashboard.module.css';
 
-
-
+import { icons } from 'react-icons/lib';
 
 const DashboardPage = () => {
 
   return (
 
     <div className={styles.background}>
-
       <div className={styles.dashboarditem}>
-
-      <div className="profile-icon">
-
+    <div className={styles.img}>
+    <img src='/vent.png' alt='s-vent' width='30px' height='30px'/> s-vent
+    </div>
+    <div className="profile-icon">
         {/* User profile icon */}
-
-        <img src="/profile-icon.png" alt="Profile" />
-
+        <img src="/profile.png" alt="Profile" height='40px' width='40px'/> Jamie Lynn
       </div>
-
-      <div className="search-bar">
+      <div className={styles.searchbar}>
 
         {/* Search bar */}
-
-        <input type="text" placeholder="Search" />
+        <input className={styles.text} type="text" placeholder="Type to Search...." />
 
       </div>
-
-      <div className="login-logout-icons">
-
+      <div className= {styles.loginlogouticons}>
         <Link href="/login">login</Link>
-
       </div>
-
+      <div className={styles.signup}>
+    <Link href="/signup">SignUp</Link>
+</div>
       </div>
-
-
-
-
       <div className={styles.dashboarditem1}>
-
       <div className={styles.container}>
-
       <div className={styles.sidebar}>
 
-        <ul>
+  <ul>
 
           <li><Link href="/dashboard">Dashboard</Link></li>
 
@@ -58,29 +46,52 @@ const DashboardPage = () => {
 
           <li><Link href="/view-users">View Users</Link></li>
 
+          <li><Link href="/contact">Contact Us</Link></li>
+
+          <li><Link href="/about">About Us</Link></li>
+
           <li><Link href="/settings">Settings</Link></li>
 
+          <li><Link href="/logout">LogOut</Link></li>
         </ul>
 
+<div>
+
+      <div className={styles.main}> BUILDINGS SORT BY:BUILDING ID
+      <div className={styles.temp}> JAMIE'S HOME AVG.TEMP </div>
+      <div className={styles.temp2}> WORK BUILDING AVG.TEMP</div>
+</div>
+      
+
+</div>
+
+      <div className={styles.dashboarditem3}>GRAPH
+
+      <div className={styles.buildgraph}> </div>
+
+
+
+
+      </div>
+
+      <div className={styles.dashboarditem4}>VENTS SORT BY:ROOM</div>
+
       </div>
 
     </div>
 
+
       </div>
 
-      <div className={styles.dashboarditem2}>Dashboard Item 3</div>
 
-      <div className={styles.dashboarditem3}>Dashboard Item 4</div>
-
-      <div className={styles.dashboarditem4}>Dashboard Item 5</div>
 
     </div>
+
 
   );
 
+
 };
-
-
 
 
 export default DashboardPage;
